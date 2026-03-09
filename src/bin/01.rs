891ace0da956de68712c6a25fb6b455ac9041e72
viktaur <victorgabal@gmail.com>
2026-03-09 21:20:34 +0100

    complete days 1 and 3

---
+++ src/bin/01.rs
@@ -0,0 +1,86 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let rotations = input.lines().map(|l| Rotation::from_str(l).unwrap());
+    // Keeps track of how many times it's zero
+    let mut n_zero = 0;
+    // Keeps track of the current value;
+    let mut curr_val = 50;
+    for r in rotations {
+        curr_val = r.apply(curr_val);
+        if curr_val == 0 {
+            n_zero += 1;
+        }
+    }
+
+    Some(n_zero)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let rotations = input.lines().map(|l| Rotation::from_str(l).unwrap());
+    // Keeps track of how many times it has pointed to zero.
+    let mut n_zero = 0;
+    // Keeps track of the current value.
+    let mut curr_val = 50;
+    for r in rotations {
+        let (new_val, visits_zero) = r.apply_with_zero_count(curr_val);
+        println!("curr_val: {curr_val} rotation: {:?} new_val: {new_val} visits_zero: {visits_zero}", r);
+        curr_val = new_val;
+        n_zero += visits_zero;
+    }
+
+    Some(n_zero)
+}
+
+#[derive(Debug)]
+enum Rotation {
+    Left(u64),
+    Right(u64)
+}
+
+impl Rotation {
+    fn from_str(s: &str) -> Option<Self> {
+        let val: u64 = s[1..].parse().ok()?;
+
+        if s.starts_with("L") {
+            Some(Rotation::Left(val))
+        } else if s.starts_with("R") {
+            Some(Rotation::Right(val))
+        } else {
+            None
+        }
+    }
+
+    fn apply(&self, val: u64) -> u64 {
+        match self {
+            Rotation::Left(x) => (val + 100 - (x % 100)) % 100,
+            Rotation::Right(x) => (val + x) % 100,
+        }
+    }
+
+    fn apply_with_zero_count(&self, val: u64) -> (u64, u64) {
+        match self {
+            Rotation::Left(x) => ((val + 100 - (x % 100)) % 100, (((100 - val) % 100) + x) / 100),
+            Rotation::Right(x) => ((val + x) % 100, (val + x) / 100),
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+}
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,17 +1,10 @@
-use itertools::Itertools;
-use itertools::EitherOrBoth;
-use std::cmp::Ordering;
-use std::collections::HashMap;
-use std::collections::HashSet;
-
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u64> {
     Some(
         input
             .lines()
-            .map(|l| find_max_joltage(l))
-            .flatten()
+            .flat_map(find_max_joltage)
             .sum()
     )
 }
@@ -20,96 +13,45 @@ pub fn part_two(input: &str) -> Option<u64> {
     Some(
         input
             .lines()
-            .map(|l| find_max_joltage_2(l))
-            .flatten()
+            .flat_map(find_max_joltage_12)
             .sum()
     )
 }
 
-fn find_max_joltage(bank: &str) -> Option<u64> {
-    let bank_chars: Vec<char> = bank.chars().collect();
-    let mut max_joltage: Option<u64> = None;
+fn find_max_joltage(line: &str) -> Option<u64> {
+    let digits: Vec<u64> = line.chars().map(|c| c.to_digit(10).unwrap() as u64).collect();
 
-    for i in 0..bank_chars.len() {
-        for j in i+1..bank_chars.len() {
-            let joltage =
-                bank_chars.get(i)?.to_digit(10)? as u64 * 10 +
-                bank_chars.get(j)?.to_digit(10)? as u64;
+    let (pos, &first) = digits[0..digits.len()-1]
+        .iter()
+        .enumerate()
+        .max_by_key(|(_, d)| *d)?;
+    let &second = digits[pos+1..].iter().max()?;
 
-            max_joltage = Some(joltage.max(max_joltage.unwrap_or(0)))
-        }
+    Some(first * 10 + second)
+}
+
+fn find_max_joltage_12(line: &str) -> Option<u64> {
+    let digits: Vec<(usize, u64)> = line
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .enumerate()
+        .collect();
+    let mut left = 12;
+    let mut min_pos = 0;
+    let mut output = Vec::new();
+
+    while left > 0 {
+        let selection = &digits[min_pos..=(digits.len()-left)];
+        let (_, d) = selection.iter().max_by_key(|(_, d)| d)?;
+        // Get the position of the first digit matching d.
+        let (pos, _) = selection.iter().find(|(_, e)| e == d)?;
+
+        output.push(d);
+        min_pos = pos + 1;
+        left -= 1;
     }
 
-    max_joltage
-}
-
-type DigitNodeMap = HashMap<usize, DigitNode>;
-
-struct DigitNode {
-    pos: usize,
-    value: u32,
-    left: Option<usize>,
-    right: Option<usize>,
-}
-
-impl DigitNode {
-    fn next
-}
-
-fn find_max_joltage_2(bank: &str) -> Option<u64> {
-    let digits = bank.chars().map(|c| c.to_digit(10).unwrap_or(0));
-    let sorted_stacks = construct_stack_tree(digits);
-    let mut selected_digits = HashSet::new();
-    find_next_digit(&sorted_stacks, &mut selected_digits, 12);
-
-    Some(
-        selected_digits
-            .iter()
-            .sorted_by(|(i_a, _), (i_b, _)| i_a.cmp(i_b)) // sort by original position
-            .map(|(_, d)| *d)
-            .fold(0, |acc, d| acc * 10 + d as u64)
-    )
-}
-
-fn construct_stack_tree(digits: impl Iterator<Item = u32>) -> (DigitNodeMap, usize) {
-    let mut stacks: Vec<Vec<(usize, u32)>> = Vec::new();
-
-    for (i, d) in digits.enumerate() {
-        match stacks.last_mut() {
-            Some(stack) => match stack.last() {
-                Some((_, e)) if d > *e => stacks.push(vec![(i, d)]), // create a new stack with d
-                _ => stack.push((i, d)),                             // append to the existing one
-            }
-            _ => stacks.push(vec![(i, d)]),
-        }
-    }
-
-    stacks.sort_by(|a, b| cmp_stack(b, a)); // Sort DESC
-    stacks
-}
-
-fn cmp_stack(stack_a: &Vec<(usize, u32)>, stack_b: &Vec<(usize, u32)>) -> Ordering {
-    for pair in stack_a.iter().zip_longest(stack_b.iter()) {
-        let (va, vb) = match pair {
-            EitherOrBoth::Both(a, b) => (a, b),
-            EitherOrBoth::Left(_) => return Ordering::Less,
-            EitherOrBoth::Right(_) => return Ordering::Greater,
-        };
-
-        if va != vb {
-            return va.1.cmp(&vb.1);
-        }
-    }
-
-    Ordering::Equal
-}
-
-fn find_next_digit(
-    sorted_stacks: &Vec<Vec<(usize, u32)>>,
-    selected_digits: &mut HashSet<(usize, u32)>,
-    n: u32
-) {
-
+    Some(output.into_iter().fold(0, |acc, e: &u64| acc * 10 + e))
 }
 
 #[cfg(test)]
